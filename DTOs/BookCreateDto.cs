da357624607fb4f4d8d858136b4c62d7576f9c7c
Duong Tan Phat <phat.duongtan.cit22@eiu.edu.vn>
2026-05-27 20:26:21 +0700

    Add Book DTO. Why DTO? Without DTO: Book represents database data. With DTO: BookCreateDto represents incoming request data. This separation is considered best practice.

---
+++ DTOs/BookCreateDto.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+
+namespace LibraryManagementSystem.DTO
+{
+    public class BookCreateDto
+    {
+        [Required]
+        public string Title { get; set; } = null!;
+
+        public string? Description { get; set; }
+
+        public string? BookCode { get; set; }
+
+        public string? Publisher { get; set; }
+
+        public DateTime? PublishedYear { get; set; }
+
+        [Required]
+        public int AuthorId { get; set; }
+
+        [Required]
+        public int CategoryId { get; set; }
+
+        public int TotalCopies { get; set; }
+
+        public int AvailableCopies { get; set; }
+
+        public IFormFile? ImageFile { get; set; }
+
+        public IFormFile? PdfFile { get; set; }
+    }
+}
