13fe0414365af6a861c456b21157be5b1cd7442b
Duong Tan Phat <phat.duongtan.cit22@eiu.edu.vn>
2026-05-27 23:02:40 +0700

    Finish Exercise 5

--- Controllers/LoansController.cs
+++ Controllers/LoansController.cs
@@ -16,15 +16,43 @@ namespace LibraryManagementSystem.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<List<Loan>>> GetAll()
+        public async Task<ActionResult<List<Loan>>> GetAll(int? userId, int? status, DateTime? fromDate, DateTime? toDate)
         {
-            return await _context.Loans.ToListAsync();
+            var query = _context.Loans
+                .Include(l => l.Book)
+                .Include(l => l.User)
+                .AsQueryable();
+
+            if (userId.HasValue)
+            {
+                query = query.Where(l => l.UserId == userId);
+            }
+
+            if (status != null)
+            {
+                query = query.Where(l => l.Status == status);
+            }
+
+            if (fromDate.HasValue)
+            {
+                query = query.Where(l => l.LoanDate >= fromDate);
+            }
+
+            if (toDate.HasValue)
+            {
+                query = query.Where(l => l.LoanDate <= toDate);
+            }
+
+            return await query.ToListAsync();
         }
 
         [HttpGet("{id}")]
         public async Task<IActionResult> GetById(int id)
         {
-            var loan = await _context.Loans.FindAsync(id);
+            var loan = await _context.Loans
+                .Include(l => l.Book)
+                .Include(l => l.User)
+                .FirstOrDefaultAsync(l => l.LoanId == id);
 
             if (loan == null)
             {
@@ -39,29 +67,58 @@ namespace LibraryManagementSystem.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> Add(Loan newLoan)
+        public async Task<IActionResult> LoanBook(Loan newLoan)
         {
+            var checkBook = await _context.Books.FindAsync(newLoan.BookId);
+
+            if (checkBook == null)
+            {
+                return NotFound("We don't know this book ");
+            }
+
+            if (checkBook.AvailableCopies <= 0)
+            {
+                return BadRequest("Sorry, we don't have any available copies of this book.");
+            }
+
+            newLoan.Status = 0;
+            newLoan.LoanDate = DateTime.Now;
             _context.Loans.Add(newLoan);
+
+            checkBook.AvailableCopies -= 1; // Decrease available copies when a loan is created
+
             await _context.SaveChangesAsync();
+
             return CreatedAtAction(nameof(GetById), new { id = newLoan.LoanId }, newLoan);
         }
 
         [HttpPut("{id}")]
-        public async Task<IActionResult> UpdateById(int id, Loan neededLoan)
+        public async Task<IActionResult> ReturnBook(int id, Loan neededLoan)
         {
             if (id != neededLoan.LoanId)
             {
                 return BadRequest("Id is not the same!");
             }
 
-            var willChangeLoan = await _context.Loans.FindAsync(id);
+            var willChangeLoan = await _context.Loans
+                .Include(l => l.Book)
+                .FirstOrDefaultAsync(l => l.LoanId == id);
 
             if (willChangeLoan == null)
             {
                 return NotFound("This loan isn't exist in database. Try another id.");
             }
 
-            //////////
+            if (willChangeLoan.Status == 1)
+            {
+                return BadRequest("This book has already been returned.");
+            }
+
+            willChangeLoan.ReturnDate = DateTime.Now;
+            willChangeLoan.Status = 1; // 1 means returned
+            
+            var book = await _context.Books.FindAsync(willChangeLoan.BookId);
+            book.AvailableCopies++; // Return a book mean availabel copies + 1
 
             await _context.SaveChangesAsync();
 
